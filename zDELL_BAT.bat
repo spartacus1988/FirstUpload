32160a44c1bd8ff4abc3ce0940d676cb9080095c
mixxxxx <spartacusxxxxx@gmail.com>
2017-04-28 19:17:12 +0300

    first commit

---
+++ zDELL_BAT.bat
@@ -0,0 +1,24 @@
+﻿echo off  
+::attrib -a -r -s -h C:\���������\������\*.*
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\offers_work.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\prototypes_work.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\upload_work.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\prototypes_work_temp.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\offers_work_temp.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\offers_0.xml
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\prototypes_0.xml
+
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\instock_work.xml
+
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\prices_work.xml
+
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\instock_work_temp.xml
+
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\prices_work_temp.xml
+
+
+del /Q C:\Bitrix\www\bitrix\components\es\FirstUpload\FirstUploadLOG.txt
+
+::deltree /y C:\���������\������\
+::del C:\...\startup\demolition.bat
+end 
